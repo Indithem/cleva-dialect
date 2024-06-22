2fe252482913ad4a035fe76dbb72f69ab598b83d
Indithem <128889127+Indithem@users.noreply.github.com>
2024-06-22 20:33:56 +0530

    arguments

---
+++ DOCS.md
@@ -0,0 +1,5 @@
+Things/preprocessors that are newly introduced in cleva.
+
+## #import
+This preprocessor searches for a cleva source file and pastes it during transpilation.
+The path is relative to the transpiler.
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use tree_sitter::Parser;
 extern crate tree_sitter_cleva;
 
-
 fn main() {
-    let mut parser = Parser::new();
+    let config: Args = Figment::new()
+        .merge(Serialized::defaults(Args::parse()))
+        .join(Toml::file("config.toml"))
+        .merge(Env::prefixed("APP_"))
+        .extract()
+        .expect("Failed to load configs");
+
+    let mut parser = tree_sitter::Parser::new();
     parser.set_language(&tree_sitter_cleva::language()).unwrap();
-    let code = r#"
-int double(int x) {
-    return x * 2;
-}
-"#;
+
+    let code = std::fs::read_to_string(config.file).expect("Failed to read file");
+
     let tree = parser.parse(code, None);
     println!("{:?}", tree);
+
+    println!("compiler used:{:?}", config.cc);
+}
+
+use clap::Parser;
+use figment::{
+    providers::{Env, Format, Serialized, Toml},
+    Figment,
+};
+
+use serde::{Deserialize, Serialize};
+#[derive(Deserialize, Serialize, clap::Parser, Debug)]
+struct Args {
+    /// The input file
+    file: String,
+    /// c compiler to use
+    #[clap(long, required = false)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cc: Option<String>,
 }
